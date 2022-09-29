51db580f5d8a71e34315697276ca727f01d9b1c5
Volodymyr <vovawestland@gmail.com>
2022-09-29 08:38:35 +0200

    Feauture is ready

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,9 @@ public class Main {
         System.out.println("Version 1.0 is ready");
         System.out.println("Änderung");
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
